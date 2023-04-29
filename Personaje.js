b698e183cc2ddf3f672cd861965a876626b4f349
Claudio <c.saez15@ufromail.cl>
2023-04-28 23:39:28 -0400

    mejoras -  generarAtaque

--- Personaje.js
+++ Personaje.js
@@ -33,18 +33,18 @@
 
     generarAtaque(clase) {
         const ataque = require("./attacks.json");
-        let a;
+        let ataqueTipo;
         if (clase == "MAGICIAN" || clase == "FAIRY") {
-            a = ataque.filter(function (ataque) {
+            ataqueTipo = ataque.filter(function (ataque) {
                 return ataque.type == "MAGIC";
             })
         } else {
-            a = ataque.filter(function (ataque) {
+            ataqueTipo = ataque.filter(function (ataque) {
                 return ataque.type == "PHYSICAL";
             })
         }
         const randomClase = Math.floor(Math.random() * clase.length);
-        return a[randomClase];
+        return ataqueTipo[randomClase];
     }
 
     generarVida() {
@@ -59,27 +59,25 @@
     // + o ${}
     atacar(enemigo) {
 
-        let mensaje = " ";
-        let linea = " "
         if (this.velocidad > enemigo.velocidad) { // quien ataca primero
             if (Math.random() >= 0.5) { //usa habilidad uno
                 if (this.ataqueUno.accuracy < Math.floor(Math.random() * (100 - 1 + 1) + 1)) { //golpea
                     enemigo.vida -= this.ataqueUno.damage; //daño
-                    mensaje += `${this.nombre} ataca con ${this.ataqueUno.name}...Da en el blanco!. La vida del ${enemigo.nombre} queda en ${enemigo.vida}`;
-                    mensaje += enemigo.contratacar(this);
+                    console.log(`${this.nombre} ataca con ${this.ataqueUno.name}...Da en el blanco!. La vida del ${enemigo.nombre} queda en ${enemigo.vida}`);
+                    enemigo.contratacar(this);
                 } else { //falla
-                    mensaje += `${this.nombre} ataca con ${this.ataqueUno.name}...Falla!. La vida del ${enemigo.nombre} se mantiene en ${enemigo.vida}`;
-                    mensaje += enemigo.contratacar(this);
+                    console.log(`${this.nombre} ataca con ${this.ataqueUno.name}...Falla!. La vida del ${enemigo.nombre} se mantiene en ${enemigo.vida}`);
+                    enemigo.contratacar(this);
                 }
 
             } else { //Usa habilidad dos
                 if (this.ataqueDos.accuracy < Math.floor(Math.random() * (100 - 1 + 1) + 1)) { //golpea
                     enemigo.vida -= this.ataqueDos.damage; //daño
-                    mensaje +=`${this.nombre} ataca con ${this.ataqueDos.name}...Da en el blanco!. La vida del ${enemigo.nombre} queda en ${enemigo.vida}`;
-                    mensaje += enemigo.contratacar(this);
+                    console.log(`${this.nombre} ataca con ${this.ataqueDos.name}...Da en el blanco!. La vida del ${enemigo.nombre} queda en ${enemigo.vida}`);
+                    enemigo.contratacar(this);
                 } else { //falla
-                    mensaje += `${this.nombre} ataca con ${this.ataqueDos.name}...Falla!. La vida del ${enemigo.nombre} se mantiene en ${enemigo.vida}`;
-                    mensaje += enemigo.contratacar(this);
+                    console.log(`${this.nombre} ataca con ${this.ataqueDos.name}...Falla!. La vida del ${enemigo.nombre} se mantiene en ${enemigo.vida}`);
+                    enemigo.contratacar(this);
                 }
             }
 
@@ -87,67 +85,57 @@
             if (Math.random() >= 0.5) {
                 if (enemigo.ataqueUno.accuracy < Math.floor(Math.random() * (100 - 1 + 1) + 1)) {
                     this.vida -= enemigo.ataqueUno.damage;
-                    mensaje += enemigo.nombre + " ataca con " + enemigo.ataqueUno.name +"...Da en el blanco!. La vida del " + this.nombre + " queda en " + this.vida;
-                    mensaje += this.contratacar(enemigo);
+                    console.log(enemigo.nombre + " ataca con " + enemigo.ataqueUno.name +"...Da en el blanco!. La vida del " + this.nombre + " queda en " + this.vida)
+                        this.contratacar(enemigo);
                 } else {
-                    mensaje += console.log(enemigo.nombre + " ataca con " + enemigo.ataqueUno.name +"...Falla!. La vida del " + this.nombre + " se mantiene en " + this.vida);
-                    mensaje += this.contratacar(enemigo);
+                    console.log(enemigo.nombre + " ataca con " + enemigo.ataqueUno.name +"...Falla!. La vida del " + this.nombre + " se mantiene en " + this.vida);
+                        this.contratacar(enemigo);
                 }
             } else {
                 if (enemigo.ataqueDos.accuracy < Math.floor(Math.random() * (100 - 1 + 1) + 1)) {
                     this.vida -= enemigo.ataqueUno.damage;
-                    mensaje += enemigo.nombre + " ataca con " + enemigo.ataqueDos.name +"...Da en el blanco!. La vida del " + this.nombre + " queda en " + this.vida;
-                    mensaje += this.contratacar(enemigo);
+                    console.log(enemigo.nombre + " ataca con " + enemigo.ataqueDos.name +"...Da en el blanco!. La vida del " + this.nombre + " queda en " + this.vida);
+                        this.contratacar(enemigo);
                 } else {
-                    mensaje += enemigo.nombre + " ataca con " + enemigo.ataqueDos.name +"...Falla!. La vida del " + this.nombre + " se mantiene en " + this.vida;
-                    mensaje += this.contratacar(enemigo);
+                    console.log(enemigo.nombre + " ataca con " + enemigo.ataqueDos.name +"...Falla!. La vida del " + this.nombre + " se mantiene en " + this.vida);
+                        this.contratacar(enemigo);
                 }
             }
 
         }
-
-        return mensaje;
     }
 
     contratacar(enemigo) {
-        let mensaje = ""
         if (Math.random() >= 0.5) { //usa habilidad uno
             if (this.ataqueUno.accuracy < Math.floor(Math.random() * (100 - 1 + 1) + 1)) { //golpea
                 enemigo.vida -= this.ataqueUno.damage; //daño
-                mensaje += `${this.nombre} ataca con ${this.ataqueUno.name}...Da en el blanco!. La vida del ${enemigo.nombre} queda en ${enemigo.vida}`;
+                console.log(`${this.nombre} ataca con ${this.ataqueUno.name}...Da en el blanco!. La vida del ${enemigo.nombre} queda en ${enemigo.vida}`);
             } else { //falla
-                mensaje +=`${this.nombre} ataca con ${this.ataqueUno.name}...Falla!. La vida del ${enemigo.nombre} se mantiene en ${enemigo.vida}`;
+                console.log(`${this.nombre} ataca con ${this.ataqueUno.name}...Falla!. La vida del ${enemigo.nombre} se mantiene en ${enemigo.vida}`);
             }
         }
          else  //Usa habilidad dos
         if (this.ataqueDos.accuracy < Math.floor(Math.random() * (100 - 1 + 1) + 1)) { //golpea
             enemigo.vida -= this.ataqueDos.damage; //daño
-            mensaje +=this.nombre + " ataca con " + this.ataqueDos.name +"...Da en el blanco!. La vida del " + enemigo.nombre + " queda en " + enemigo.vida;
+            console.log(this.nombre + " ataca con " + this.ataqueDos.name +"...Da en el blanco!. La vida del " + enemigo.nombre + " queda en " + enemigo.vida)
         } else { //falla
-            mensaje +=this.nombre + " ataca con " + this.ataqueDos.name +"...Falla!. La vida del " + enemigo.nombre + " se mantiene en " + enemigo.vida;
+            console.log(this.nombre + " ataca con " + this.ataqueDos.name +"...Falla!. La vida del " + enemigo.nombre + " se mantiene en " + enemigo.vida);
         }
-
-        return " "+mensaje;
-    }
-
-    batalla(){
-        let a = new Personaje("ss");
-        let b = new Personaje("dedfed");
-        console.log(a);
     }
 }
 
+   
 
 let a = new Personaje("gg");
 let b = new Personaje("Bggg");
 
 
 function batalla(a,b){
-    let txt = "";
+    
     while(a.estaVivo() && b.estaVivo()){
-        txt =txt+"\n"+a.atacar(b);
+        a.atacar(b);
     }
-    console.log(txt);
+    
 }
 
 function comenzar(personajeUno, personajeDos){
